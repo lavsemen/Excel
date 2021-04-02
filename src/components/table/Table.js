940c2deab058cc1ff8469309af0b8977348c5ae1
Semen <lavsemen2108@gmail.com>
2021-04-02 23:53:52 +0500

    last resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,7 @@
 import {ExecelComponent} from '@core/ExecelComponent'
 import {createTable} from './table.template'
-import {$} from '@core/dom'
+import {resizeTable} from './table.resize'
+import {shouldResize} from './table.function'
 
 export class Table extends ExecelComponent {
   static className = 'excel__table';
@@ -24,74 +25,9 @@ export class Table extends ExecelComponent {
     }
   }
 
-  onMousedown(e) {
-    if (e.target.dataset.resize) {
-      const $resizer = $(e.target)
-      const $perent = $resizer.closest('[data-type="resizeble"]')
-      const cords = $perent.getCoords()
-      const perentData = $perent.data.collIndex
-      const cells = this.$root.findAll(`[data-cell-index="${perentData}"]`)
-      const type = $resizer.data.resize
-
-
-      $resizer.css({
-        opacity: 1,
-        width: type === 'row'
-                         ? document.documentElement.clientWidth + 'px'
-                         : null,
-        height: type === 'col'
-                         ? document.documentElement.clientHeight + 'px'
-                         : null
-      })
-
-      let result;
-      const resizeElement = (e) => {
-        if (type === 'col') {
-          const delta = e.pageX - cords.right
-          const value = cords.width + delta
-
-          result = value + 'px'
-          $resizer.css({
-            right: -delta + 'px',
-          })
-        } else {
-          const delta = e.clientY - cords.bottom
-          const value = cords.height + delta
-          result = value + 'px'
-
-          $resizer.css({
-            bottom: -delta + 'px',
-          })
-        }
-      }
-
-      document.onmousemove = e => {
-        resizeElement(e)
-      }
-
-      document.onmouseup = e => {
-        document.onmousemove = null
-        document.onmouseup = null
-
-        $resizer.css({
-          width: type === 'col'
-                      ? $perent.clientHeight
-                      :null,
-          height: type === 'row'
-                      ? $perent.clientWidth
-                      :null,
-
-          right: 0,
-          bottom: 0,
-          opacity: 0,
-        })
-        if (type === 'col') {
-          $perent.css({width: result})
-          cells.forEach(item => item.style.width = result)
-        } else {
-          $perent.css({height: result})
-        }
-      }
+  onMousedown(event) {
+    if (shouldResize(event)) {
+      resizeTable(event, this.$root)
     }
   }
 }
